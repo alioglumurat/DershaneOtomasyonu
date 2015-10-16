ba963f9945034a8daafb8a4452b598157cfc0227
Hakan ÖZER <hakanozer02@gmail.com>
2015-10-16 14:58:33 +0300

    Proje Başlangıç

---
+++ DB.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace otomasyon
+{
+    class DB
+    {
+        private String dataSource = "OMER-PC";
+        private String database = "wissenProject";
+        private String userId = "sa";
+        private String pass = "54321";
+
+        SqlConnection con;
+
+        public DB()
+        {
+            try
+            {
+                con = new SqlConnection("Data Source="+dataSource+"; Database="+database+"; Persist Security Info=False; User ID="+userId+"; Password="+pass+"");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı tanımlama hatası");
+              
+            }
+        }
+
+        public SqlConnection baglan()
+        {
+            try
+            {
+                if (con.State==ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Bağlantı açma hatası");
+            }
+
+            return con;
+        }
+
+        public void kapat()
+        {
+            try
+            {
+                if (con.State==ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı kapatma hatası");
+              
+            }
+        }
+
+        public int userState(String username, String password)
+        {
+            int state=-1;
+
+            try
+            {
+                SqlCommand islem = new SqlCommand("users", baglan());
+                islem.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ax = new SqlParameter("@userName",SqlDbType.VarChar,20);                
+                ax.Direction = ParameterDirection.Input;
+                ax.Value = username;
+                islem.Parameters.Add(ax);
+
+                SqlParameter bx = new SqlParameter("@password", SqlDbType.VarChar, 32);
+                bx.Direction = ParameterDirection.Input;
+                bx.Value = password;
+                islem.Parameters.Add(bx);
+
+                SqlDataReader oku = islem.ExecuteReader();
+
+                if (oku.Read())
+                {
+                    state = Convert.ToInt32(oku["State"]);
+                    MessageBox.Show("Sisteme hoşgeldiniz");
+                }
+                else
+                {
+                    MessageBox.Show("Hatalı giriş yaptınız");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Giriş kontrol hatası"+ex);
+            }
+            return state;
+        }
+
+        public int userSave(String username, String password, String state)
+        {
+            int sayi = 0;
+            try
+            {
+                SqlCommand islem = new SqlCommand("userSave",baglan());
+                islem.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ax = new SqlParameter("@username", SqlDbType.VarChar, 20);
+                ax.Direction = ParameterDirection.Input;
+                ax.Value = username;
+                islem.Parameters.Add(ax);
+
+                SqlParameter bx = new SqlParameter("@pass", SqlDbType.VarChar, 32);
+                bx.Direction = ParameterDirection.Input;
+                bx.Value = password;
+                islem.Parameters.Add(bx);
+
+                SqlParameter cx = new SqlParameter("@state", SqlDbType.Char);
+                cx.Direction = ParameterDirection.Input;
+                cx.Value = state;
+                islem.Parameters.Add(cx);
+
+                sayi = islem.ExecuteNonQuery();
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kayıt işleminde hata" + ex);
+                sayi = -1;
+              
+            }
+            return sayi;
+        }
+       
+        public string MD5Sifrele(string metin)
+        {
+            
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+         
+            byte[] btr = Encoding.UTF8.GetBytes(metin);
+            btr = md5.ComputeHash(btr);
+
+          
+            StringBuilder sb = new StringBuilder();
+
+
+            foreach (byte ba in btr)
+            {
+                sb.Append(ba.ToString("x2").ToLower());
+            }
+
+        
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ kaydetkul.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace otomasyon
+{
+    public partial class kaydetkul : Form
+    {
+        public kaydetkul()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+
+            int sayi = db.userSave(txt_username.Text, db.MD5Sifrele(txt_password.Text), txt_state.Text);
+            if (sayi>0)
+            {
+                MessageBox.Show("Kayıt işlemi gerçekleştirildi");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt işleminde hata");
+            }
+
+            db.kapat();
+
+            this.Hide();
+            Form1 frm = new Form1();
+            frm.Show();
+
+        }
+    }
+}
